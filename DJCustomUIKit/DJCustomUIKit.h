9e97cf3f347b652ffb678796737b99fb64e0a08d
Jaesun <jaeysun@163.com>
2020-06-28 11:23:12 +0800

    初始化kit，添加自定义跑马灯

---
+++ DJCustomUIKit/DJCustomUIKit.h
@@ -0,0 +1,13 @@
+//
+//  DJMarqueeView.h
+//  DJCustomUIKit
+//
+//  Created by Jaesun on 2020/6/28.
+//  Copyright © 2020 jaeysun. All rights reserved.
+//
+
+#ifndef DJMarqueeView_h
+#define DJMarqueeView_h
+
+
+#endif /* DJMarqueeView_h */
